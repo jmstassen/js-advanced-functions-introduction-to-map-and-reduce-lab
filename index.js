62541978346a5385fb964c16957196aa3771d272
John M. Stassen <john.stassen@gmail.com>
2021-08-02 14:40:34 -0700

    commit tree a20b231b35d5e715af96819a9086f9aa97e35f7b

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let newArray = []
+    for (let i = 0; i<sourceArray.length; i++) {
+      let newValue = sourceArray[i] * -1
+      newArray.push(newValue)
+    }
+    return newArray
+}
+
+function mapToNoChange(sourceArray) {
+    let newArray = []
+    for (let i = 0; i<sourceArray.length; i++) {
+      let newValue = sourceArray[i]
+      newArray.push(newValue)
+    }
+    return newArray
+}
+
+function mapToNoChange(sourceArray) {
+    let newArray = []
+    for (let i = 0; i<sourceArray.length; i++) {
+      let newValue = sourceArray[i]
+      newArray.push(newValue)
+    }
+    return newArray
+}
+
+function mapToDouble(sourceArray) {
+    let newArray = []
+    for (let i = 0; i<sourceArray.length; i++) {
+      let newValue = sourceArray[i] * 2
+      newArray.push(newValue)
+    }
+    return newArray
+}
+
+function mapToSquare(sourceArray) {
+    let newArray = []
+    for (let i = 0; i<sourceArray.length; i++) {
+      let newValue = sourceArray[i] * sourceArray[i]
+      newArray.push(newValue)
+    }
+    return newArray
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i<sourceArray.length; i++) {
+      let newTotal = total + sourceArray[i] 
+      total = newTotal
+    }
+    return total
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i<sourceArray.length; i++) {
+      if (!sourceArray[i]) {
+      return false}
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i<sourceArray.length; i++) {
+      if (sourceArray[i]) {
+      return true}
+    }
+    return false
+}
